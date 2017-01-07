fe121dd9971250c283c473e156c9fc3c7407f99a
Tan Moy <Tan Moy>
2017-01-07 19:43:46 +0530

    v1

---
+++ annoymatic.js
@@ -0,0 +1,5 @@
+var userAns = "";
+while (userAns !== "yes" && userAns !== "yeah"){
+    userAns = prompt("Are we there yet?")
+}
+alert("Yay, We finally made it.");
---
+++ script.js
@@ -0,0 +1,3 @@
+var userAge = prompt("What is your age?");
+var days = Math.ceil((userAge*365) + (userAge/4))
+alert("You have lived " + days + " days");
