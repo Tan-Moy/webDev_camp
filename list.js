af36a94d2b2c37e4f7b5bef2684aa6cd94afdb6a
Tan Moy <Tan Moy>
2017-01-08 23:22:08 +0530

    v3

--- list.js
+++ list.js
@@ -3,27 +3,45 @@ var quit = "";
 do {
     var input = prompt("What would like to do?");
     if (input === "list") {
-        console.log("**********")
-        todos.forEach(function(todo,i) {
-            console.log(i +": "+todo);
-        });
-        console.log("**********")
+        listTodos();
     }
     else if (input === "new") {
-        var add = prompt("What would you like to add?");
-        todos.push(add);
+        newTodos();
     }
     else if (input === "quit") {
-        quit = "yes"
-        console.log("Closing the app...")
+        exit();
     }
-    else if(input === "delete"){
-        var index = prompt("Enter the index of to-do to delete");
-        todos.splice(index,1);
-        console.log("deleted");
+    else if (input === "delete") {
+        deleteTodos();
     }
     else {
-        console.log("Input either \"list\",\"new\",\"quit\", or \"delete\".")
+        console.log("Input either \"list\", \"new\", \"quit\", or \"delete\".")
     }
 } while (quit !== "yes")
 console.log("Goodbye")
+
+//refactoring
+
+function listTodos() {
+    console.log("**********")
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("**********")
+}
+
+function newTodos() {
+    var add = prompt("What would you like to add?");
+    todos.push(add);
+}
+
+function deleteTodos() {
+    var index = prompt("Enter the index of to-do to delete");
+    todos.splice(index, 1);
+    console.log("deleted");
+}
+
+function exit(){
+quit = "yes"
+        console.log("Closing the app...")
+}
