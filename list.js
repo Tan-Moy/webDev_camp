9b7a5d811231a3ce29d7da2c8807c270917ab59b
Tan Moy <Tan Moy>
2017-01-08 15:43:56 +0530

    v2

--- list.js
+++ list.js
@@ -1,9 +1,20 @@
 var todos = [];
-var input = prompt("What would like to do?");
-if (input === "list") {
-    console.log(todos);
-}
-else if(input === "new"){
-var add = prompt("What would you like to add?");
-todos.push(add);
-}
+var quit = "";
+do {
+    var input = prompt("What would like to do?");
+    if (input === "list") {
+        console.log(todos);
+    }
+    else if (input === "new") {
+        var add = prompt("What would you like to add?");
+        todos.push(add);
+    }
+    else if (input === "quit") {
+        quit = "yes"
+        console.log("Closing the app...")
+    }
+    else {
+        console.log("Input either \"list\" or \"new\" or \"quit\".")
+    }
+} while (quit !== "yes")
+console.log("Goodbye")
