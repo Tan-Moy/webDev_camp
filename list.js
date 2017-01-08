31dea44fc68d56ad65c0f2f9d7ac2aec6abe8f7f
Tan Moy <Tan Moy>
2017-01-08 21:43:25 +0530

    v3

--- list.js
+++ list.js
@@ -3,7 +3,11 @@ var quit = "";
 do {
     var input = prompt("What would like to do?");
     if (input === "list") {
-        console.log(todos);
+        console.log("**********")
+        todos.forEach(function(todo,i) {
+            console.log(i +": "+todo);
+        });
+        console.log("**********")
     }
     else if (input === "new") {
         var add = prompt("What would you like to add?");
@@ -13,8 +17,13 @@ do {
         quit = "yes"
         console.log("Closing the app...")
     }
+    else if(input === "delete"){
+        var index = prompt("Enter the index of to-do to delete");
+        todos.splice(index,1);
+        console.log("deleted");
+    }
     else {
-        console.log("Input either \"list\" or \"new\" or \"quit\".")
+        console.log("Input either \"list\",\"new\",\"quit\", or \"delete\".")
     }
 } while (quit !== "yes")
 console.log("Goodbye")
